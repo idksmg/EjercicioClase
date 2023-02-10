e1cd9fe532467dd5d5a9933448da7fbbf74c7da4
idksmg <111992372+idksmg@users.noreply.github.com>
2023-02-10 07:29:22 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Prueba prueba = new Prueba();
+        System.out.println("El resultado es: "+prueba.Suma());
+    }
+}
