87bdc2e8600321013e5af51b34b492e594e25d37
JEmilioRey1021 <119058559+JEmilioRey1021@users.noreply.github.com>
2023-02-10 08:41:00 -0600

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-
-        Prueba prueba = new Prueba();
-        System.out.println("El resultado es: "+prueba.Suma());
-    }
-}
